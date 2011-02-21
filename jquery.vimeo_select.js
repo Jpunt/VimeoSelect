1e5e7b1f3573d138d9168db89732f0e49c3e1f6c
Jasper Haggenburg <jasper@jpunt.nl>
2011-02-21 22:38:14 +0100

    Added error-message on failing Vimeo-API

--- jquery.vimeo_select.js
+++ jquery.vimeo_select.js
@@ -14,10 +14,10 @@
 		var defaults 	= {
 			api_url:'http://vimeo.com/api/v2/',
 			thumbnail_size: 'medium',
-			text_help: 'Select video'
+			text_help: 'Select video:'
 		};
 
-		var settings 	= $.extend(defaults, options);
+		var settings = $.extend(defaults, options);
 		console.log('settings:');
 		console.log(settings);
 		
@@ -25,10 +25,31 @@
 		var $container	= $input.wrap('<div></div>').parent('div').addClass('vs_container');
 		var $videos 		= $('<div></div>').addClass('vs_videos').addClass(settings.thumbnail_size).hide();
 		$container.append($videos);
-				
+
+		//Add close-button
+		var $close = $('<a>x</a>').addClass('vs_close').click(function() {
+			$videos.hide();
+		});
+		$videos.append($close);
+		
+		//Add help-title
+		var $help = $('<h1>'+settings.text_help+'</h1>').addClass('vs_help');
+		$videos.append($help);
+		
 		//Get video's
-		$.getJSON(settings.api_url + settings.user_id + '/videos.json', function(data) {				
-			$.each(data, function(i, video) {
+		var status;
+		$.ajax({
+			url: settings.api_url + settings.user_id + '/videos.json', 
+			dataType: 'json',			
+			success: setupVideos,
+			complete: function(req, status) {
+				status = req.status;
+				if(status!=200) $help.text("Something went wrong while fetching video's");
+			}
+		});
+		
+		function setupVideos(videos) {
+			$.each(videos, function(i, video) {
 				$video = $('<div></div>').addClass('vs_video').click(function() {
 					$input.val(video.id);
 					$videos.hide();
@@ -36,25 +57,15 @@
 				$videos.append($video);				
 				$('<p>'+video.title+'</p>').addClass('vs_title').appendTo($video);
 				$('<img alt="thumbnail" />').attr('src', video['thumbnail_'+settings.thumbnail_size]).addClass('vs_thumbnail').appendTo($video);					
-			});			
-		});
+			});
+		};		
 		
 		//Show video's
 		$(this).focus(function() {
 			$videos.show();
 		});
-		
-		//Add close-button
-		$close = $('<a>x</a>').addClass('vs_close').click(function() {
-			$videos.hide();
-		});
-		$videos.append($close);
-		
-		//Add help-title
-		$('<h1>'+settings.text_help+'</h1>').addClass('vs_help').appendTo($videos);
 
 		//tmp
 		$videos.show();
-		
 	};
 })(jQuery);
