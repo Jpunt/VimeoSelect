3e9b7e64d6ca1009ee50aadb04122785c312f9c7
Jasper Haggenburg <jasper@jpunt.nl>
2012-12-15 16:34:10 +0100

    added cssmin to buildscript

--- grunt.js
+++ grunt.js
@@ -67,6 +67,6 @@ module.exports = function(grunt) {
   });
 
   // Default task.
-  grunt.registerTask('default', 'lint qunit concat min');
+  grunt.registerTask('default', 'lint qunit concat min cssmin');
 
 };
