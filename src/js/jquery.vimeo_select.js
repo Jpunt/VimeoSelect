e71030ef469bf472c73d8a1aabf086ee56c3f028
Jasper Haggenburg <jasper@jpunt.nl>
2012-12-15 15:39:40 +0100

    made this chainable

--- src/js/jquery.vimeo_select.js
+++ src/js/jquery.vimeo_select.js
@@ -10,28 +10,21 @@
 
 	"use strict";
 
-	$.fn.vimeo_select = function(user_id, options) {
+	$.vimeo_select = function(input, user_id, options) {
+		var self = this;
+
+		self.$input = $(input);
+		self.$input.data("vimeo_select", self);
+		self.options = $.extend({}, $.vimeo_select.defaultOptions, options);
+		self.user_id = user_id;
 
 		if(!user_id) {
-			window.alert('No user_id given');
+			window.console.log('No user_id given');
 			return false;
 		}
-			
-		var defaults = {
-			api_url:'http://vimeo.com/api/v2/',
-			thumbnail_size: 'medium',
-			hide_on_blur: true,
-			remote_thumbnail_input: false,
-			show: function() {}
-		};
 
-		var self = this;
-				self.settings = $.extend(defaults, options);
-				self.user_id	= user_id;
-				self.$input		= $(this);
-		
 		self.init = function() {
-			self.$container = $('<div></div>').addClass('vs-container').addClass(self.settings.thumbnail_size).hide();
+			self.$container = $('<div></div>').addClass('vs-container').addClass(self.options.thumbnail_size).hide();
 			self.$input.after(self.$container);
 
 			self.$close = $('<a href="#">x</a>').addClass('vs-close');
@@ -48,7 +41,7 @@
 			self.$container.append(self.$videos);
 
 			self.$input.focus(self.show);
-			if(self.settings.hide_on_blur === true) {
+			if(self.options.hide_on_blur === true) {
 				self.$input.blur(self.hide);
 			}
 
@@ -57,7 +50,7 @@
 
 		self.load = function() {
 			$.ajax({
-				url: self.settings.api_url + self.user_id + '/videos.json',
+				url: self.options.api_url + self.user_id + '/videos.json',
 				dataType: 'jsonp',
 				success: function(data) {
 					$.each(data, function(i, video) {
@@ -65,8 +58,8 @@
 							self.select($(e.currentTarget), video);
 						});
 						self.$videos.append($video);
-						$('<img alt="thumbnail" />').attr('src', video['thumbnail_'+self.settings.thumbnail_size]).addClass('vs-thumbnail').appendTo($video);
-						$('<p>'+video.title+'</p>').addClass('vs-title').appendTo($video);
+						$('<img alt="thumbnail" />').attr('src', video['thumbnail_'+self.options.thumbnail_size]).addClass('vs-thumbnail').appendTo($video);
+						$('<p>'+video.title+'</p>').addClass('vs-video-title').appendTo($video);
 					});
 					self.$videos.find('.vs-video:last').addClass('last');
 				},
@@ -82,14 +75,13 @@
 			self.$videos.find('.vs-video').removeClass('selected');
 			$video.addClass('selected');
 			self.$input.val(video.id);
-			if(self.settings.remote_thumbnail_input !== false) {
-				self.settings.remote_thumbnail_input.val(video.thumbnail_large);
+			if(self.options.remote_thumbnail_input !== false) {
+				self.options.remote_thumbnail_input.val(video.thumbnail_large);
 			}
 		};
 
 		self.show = function() {
 			self.$container.slideDown(150);
-			self.settings.show();
 		};
 
 		self.hide = function() {
@@ -97,6 +89,19 @@
 		};
 
 		self.init();
-		return self;
 	};
+
+	$.vimeo_select.defaultOptions = {
+		api_url:'http://vimeo.com/api/v2/',
+		thumbnail_size: 'medium',
+		hide_on_blur: true,
+		remote_thumbnail_input: false
+	};
+
+	$.fn.vimeo_select = function(user_id, options){
+		return this.each(function(){
+			(new $.vimeo_select(this, user_id, options));
+		});
+	};
+
 })(jQuery);
